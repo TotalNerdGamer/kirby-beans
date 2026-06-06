084ea5df86a8620a2c96d4cd15bc012234b73cb0
CelesteGamer2012 <cthomson1936@outlook.com>
2026-06-06 19:25:44 -0400

    I updated myimg to have a get_rect function!

--- src/classes/myimg.py
+++ src/classes/myimg.py
@@ -4,11 +4,13 @@ class myimg:
     def __init__(self,x,y,img):
         self.pos = pygame.Vector2(x,y)
         self.img = img
-    def draw(self,Surface):
         self.imgsurf = pygame.image.load(self.img).convert()
+    def draw(self,Surface):
         pygame.Surface.blit(Surface,self.imgsurf,self.pos)
     def update(self, screen):
         if self.pos.x + 1280 < -1:
             self.pos.x = 2560 + self.pos.x
         elif self.pos.x > 1281:
             self.pos.x = self.pos.x - 2560
+    def get_rect(self):
+        return self.imgsurf.get_rect()
