e888c0704faa7f07ebc880c55d6730d559d7ed5d
CelesteGamer2012 <cthomson1936@outlook.com>
2026-06-08 09:50:25 -0400

    Added documentation for myPos.

--- src/classes/mypos.py
+++ src/classes/mypos.py
@@ -1,5 +1,6 @@
 from pygame import Vector2
 class myPos:
+    "A tracker for a position. Used to measure scrolling."
     def __init__(self,x,y):
         self.pos=Vector2(x,y)
     def draw(self,unused):
