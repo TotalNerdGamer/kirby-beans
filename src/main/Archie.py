cc740732d2da4768ec46e25138078dfbc8a6474e
CelesteGamer2012 <134813229+TotalNerdGamer@users.noreply.github.com>
2026-06-16 14:44:43 -0400

    Updated Archie.py
    Room ID #2 now corresponds to the Debug Room.

--- src/main/Archie.py
+++ src/main/Archie.py
@@ -2,6 +2,7 @@ from TitleScreen import TitleScene
 from levels.ktchn.ktchn01 import Kitchen01
 import pygame
 
+from levels.DebugScene import DebugScene
 from src.main.DeathScene import DeathScene
 
 pygame.init()
@@ -18,7 +19,7 @@ def switchscene(i):
     scene = scenes[sceneindex]
     scene.__init__(switchscene)
     #print(scene)
-scenes = [TitleScene(switchscene),DeathScene(switchscene),Kitchen01(switchscene)]
+scenes = [TitleScene(switchscene),DeathScene(switchscene),DebugScene(switchscene),Kitchen01(switchscene)]
 sceneindex = 0
 dt = 0
 scene = scenes[sceneindex]
