dc2e0bab67aee17404a54c9501fb66a4f3844471
CelesteGamer2012 <cthomson1936@outlook.com>
2026-06-16 17:15:53 -0400

    Updated the INS to allow multiple aliases for scenes.
    All scene names passed to the function when attempting to find an id should be Title Case, because the system is case sensitive. If no scene is found by the given name, it returns 0, aka the title screen.

--- src/main/INS.py
+++ src/main/INS.py
@@ -1,3 +1,10 @@
 def resolvename(name):
-    scenes = ["Title Scene", "Death Scene","Debug Room","Kitchen 01"]
-    return scenes.index(name)
+    scenes = [["Title Scene","Title Screen","Title"], ["Death Scene","Death","Death Screen"],["Debug","Debug Area","Debug Room"],["Kitchen 01","Kitchen Level 1"]]
+    for x in range(len(scenes)):
+        i = scenes[x]
+        if type(i) == type(["hi","hi"]):
+            if name in i:
+                return x
+        elif name == i:
+            return x
+    return 0
