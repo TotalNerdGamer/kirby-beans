05693dcff9dabf905f3b2fcde11c64e73978c8b6
CelesteGamer2012 <134813229+TotalNerdGamer@users.noreply.github.com>
2026-06-16 14:41:44 -0400

    Created DebugScene.py
    It’s just a copy of Ktchn01 right now, but we can use it to test features.

---
+++ src/levels/DebugScene.py
@@ -0,0 +1,19 @@
+import sys
+import os
+sys.path.append(os.path.abspath("./src"))
+from classes.Player import Player
+from classes.Platform import Platform
+from classes.mypos import myPos
+import pygame
+from classes.myimg import myimg
+from classes.cheese import Cheese
+from classes.hudheart import HUDHeart
+from classes.mousetrap import Mousetrap
+from classes.LevelScene import LevelScene
+class DebugScene(LevelScene):
+    def __init__(self,switchscene):
+        platforms = [Platform(1280,700,2560,40,"green"),Platform(1320,460,400,40,"green"),Platform(3590,700,500,40),Platform(1320,680-11,160,22),Platform(1160,680-11,160,22),Platform(1480,680-11,160,22)]
+        colls = [Cheese(1320,400)]
+        hazards = [Mousetrap(1320,620),Mousetrap(1160,620),Mousetrap(1480,620)]
+        super().__init__(switchscene,platforms,colls,hazards,50,600,3840)
+        self.bg = "cabinets"
