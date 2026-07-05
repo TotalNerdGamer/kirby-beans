88f1e8076a3b2a3c1c2d17e14f32cd40cab570b8
CelesteGamer2012 <cthomson1936@outlook.com>
2026-07-05 14:06:11 -0400

    Updated platform to support being invisible.

--- src/classes/Platform.py
+++ src/classes/Platform.py
@@ -2,12 +2,14 @@ import pygame
 
 class Platform:
     "The titular element of the genre."
-    def __init__(self, x,y,width,height,col="green",type="grass"):
+    def __init__(self,x,y,width,height,col="green",type="grass"):
         self.pos = pygame.Vector2(x,y)
         self.size = pygame.Vector2(width,height)
         self.col = col
         self.type = type
     def draw(self,Surface):
-        pygame.draw.rect(Surface,self.col,pygame.Rect(self.pos.x-self.size.x/2,self.pos.y-self.size.y/2,self.size.x,self.size.y))
+        if not self.col == None:
+            pygame.draw.rect(Surface,self.col,pygame.Rect(self.pos.x-self.size.x/2,self.pos.y-self.size.y/2,self.size.x,self.size.y))
+        pass
     def get_rect(self):
         return pygame.Rect(self.pos.x-self.size.x/2,self.pos.y-self.size.y/2,self.size.x,self.size.y)
