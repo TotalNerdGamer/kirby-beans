8f2e06615c990512f225f28403fba04e4a1a7449
CelesteGamer2012 <cthomson1936@outlook.com>
2026-08-01 14:30:45 -0400

    Moved ALL of the classes to one classes.py file, for ease of importing.
    Now comes the hard part - rewriting all the EXISTING import statements.

--- src/classes/TLPlatform.py
+++
@@ -1,6 +0,0 @@
-from classes.Platform import Platform
-
-class TLPlatform(Platform):
-    "A shift in genre."
-    def __init__(x,y,w,h,col="green",type="grass"):
-        super().__init__(x+w/2,y+h/2,w,h,col,type)
