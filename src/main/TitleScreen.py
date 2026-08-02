8f8739af014d3ead7fa169e543ed55141f4f6888
CelesteGamer2012 <cthomson1936@outlook.com>
2026-08-02 10:21:53 -0400

    Removed a couple other useless imports.
    Also updated .gitignore.

--- src/main/TitleScreen.py
+++ src/main/TitleScreen.py
@@ -2,13 +2,9 @@ from __future__ import annotations
 import pygame
 import sys
 import os
-
-
-
 sys.path.append(os.path.abspath("./src"))
 from classes import *
 from functools import partial
-from helpfulstuff.help import *
 class TitleScene(Scene):
     "The title screen scene. I'm a poet and I didn't even know it!"
     def __init__(self,switchscene):
